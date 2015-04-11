0ed351acf1430b29c37402399ef3023906b1174c
Yitao Zhang <zhangyitao91@gmail.com>
2015-04-11 18:36:15 +0800

    Initial Commit

---
+++ ChallengeTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChallengeTable
+//
+//  Created by Yitao Zhang on 4/11/15.
+//  Copyright (c) 2015 Yitao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
